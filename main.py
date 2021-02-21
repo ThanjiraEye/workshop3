87778f6b57f7d7ec05faed62d216a47f4b7ce465
ThanjiraEye <thanjira.j@ku.th>
2021-02-21 20:37:19 +0700

    add new foder in 21/2/2021

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel, Field
+from starlette.responses import JSONResponse
+from typing import Dict, Optional, List, Tuple
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return JSONResponse(content={"message": " Hello,  World"}, status_code=200)
+
+
+@app.get("/example/")
+def get_query_parameter(start: int = 0, limit: int = 0):
+    return JSONResponse(
+        content={"message": f"start: {start} limit: {limit}"},
+        status_code=200,
+    )
+
+
+@app.get("/profile/{name}")
+def get_path_parameter(name: str):
+    return JSONResponse(
+        content={"message": f"My name is : {name}"},
+        status_code=200,
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=3000, reload=True)
